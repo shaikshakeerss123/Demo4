35b34c32fdc3490fb4d57168a781974fa8d2f3f6
shaikshakeerss123 <shaikshakeerss123@gmail.com>
2024-10-26 23:23:24 +0530

    First C

---
+++ src/test/java/day1/Prb1.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Prb1 {
+
+	public static void main(String[] args) {
+		System.out.println("Iam trying another one");
+
+	}
+
+}
---
+++ src/test/java/day1/Prb2.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Prb2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is a sample 2");
+
+	}
+
+}
